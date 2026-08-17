169e01e2981fb5695ebf111db3013ed47d86575f
unknown <25114290096@ACADEMICO.IESB.BR>
2026-08-17 20:29:59 -0300

    pratica03

--- praticas/pratica03/app-atividade-aula-tarefas/App.js
+++ praticas/pratica03/app-atividade-aula-tarefas/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { rotulo_btn_cadastro_meta, rotulo_input_meta, rotulo_lista_metas } from './mensagens';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+
+      <TextInput
+        style={styles.inputText}
+        placeholder={rotulo_input_meta}
+      />
+
+      <Button
+        title={rotulo_btn_cadastro_meta}
+      />
+
+      <Text>
+        {rotulo_lista_metas}
+      </Text>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30,
+  },
+
+  inputText: {
+    borderColor: '#8f0d0d',
+    borderWidth: 1,
+    width: 150,
+    height: 40,
+    marginBottom: 10,
   },
 });
